655d3064289bca910f7baeb8b833279522bcc01c
LMacchi <lau.macchi@gmail.com>
2018-09-25 23:44:12 -0500

    Add flag to curl to follow redirect

--- provisioners.tf
+++ provisioners.tf
@@ -45,7 +45,7 @@ locals {
       "echo '*.${var.domain}' | sudo tee /etc/puppetlabs/puppet/autosign.conf",
 
       # Download the Puppet Enterprise installer
-      "curl --progress-bar --max-time 300 -o pe-installer.tar.gz \"${var.pe_source_url}\" && tar -xzf pe-installer.tar.gz",
+      "curl -L --progress-bar --max-time 300 -o pe-installer.tar.gz \"${var.pe_source_url}\" && tar -xzf pe-installer.tar.gz",
 
       # Install Puppet enterprise
       "cat > pe.conf <<-EOF",
